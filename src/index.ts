e43851a399a382df003cf154c6486427b323f0a5
Do Lam Binh Minh <minhdlb@blockchainlabs.asia>
2021-02-23 22:45:17 +0700

    Update config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import 'module-alias/register';
 import app from '@src/app';
 import dotnev from 'dotenv';
 dotnev.config();
 const port = process.env.PORT || 2021;
 const server = app.listen(port, () => {
-    console.log(`Server starting on port ${port}`)
+    console.log(`Server starting on url: http://127.0.0.1:${port}`)
 });
 export default server;
